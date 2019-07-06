68c805d1d51ce7875cbd75173aeb786940a24b65
Bongekce <52604344+Bongekce@users.noreply.github.com>
2019-07-06 22:13:31 +0700

    Add files via upload

---
+++ document.js
@@ -0,0 +1,16 @@
+//<![CDATA[
+var key = window.location.href.split("gp/product")[1].replace("/","")
+var urls={
+    {
+'asin':'https://www.amazon.co.uk/gp/product/B076FTRGV4/?tag=mbahbejo-21',
+    'gplus':'https://plus.google.com/+KangIsmet1',
+    'twitter':"https://twitter.com/kangismetdotnet" /* yang terakhir tanpa koma */
+}
+if(key){
+    if(urls[key]){
+        window.location.href=urls[key]
+    }else{
+        document.write("'"+key+"' not found :(");
+    }
+}
+//]]>
