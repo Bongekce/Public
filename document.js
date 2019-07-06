91db461137f74d32003054ce07002081c7876d0c
Bongekce <52604344+Bongekce@users.noreply.github.com>
2019-07-06 22:16:18 +0700

    Update document.js

--- document.js
+++ document.js
@@ -2,7 +2,7 @@
 var key = window.location.href.split("gp/product")[1].replace("/","")
 var urls={
     {
-'asin':'https://www.amazon.co.uk/gp/product/B076FTRGV4/?tag=mbahbejo-21',
+'asin':'https://google.com',
     'gplus':'https://plus.google.com/+KangIsmet1',
     'twitter':"https://twitter.com/kangismetdotnet" /* yang terakhir tanpa koma */
 }
